a568f41385a4f8eab26aa4e60035ec2bcd087041
KrzysioMisio <93145787+KrzysioMisio@users.noreply.github.com>
2023-04-13 15:48:58 +0200

    Create cleanUrl.php

---
+++ cleanUrl.php
@@ -0,0 +1,10 @@
+function cleanUrl($str, $replace = [], $separator = '-') {
+	if (!empty($replace)) {
+        $str = str_replace((array)$replace, ' ', $str);
+    }
+    $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+    $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+    $clean = strtolower(trim($clean, '-'));
+    $clean = preg_replace("/[\/_|+ -]+/", $separator, $clean);
+    return $clean;
+}
